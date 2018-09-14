a980c6e9d7ba7224595b4ddef06204f116d6b1f2
ProfePosh <hpbala@umich.edu>
2018-09-13 23:40:34 -0400

    chore()

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/prototyping'));
+app.use(express.static('./dist/data-export'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/prototyping/index.html'));
+res.sendFile(path.join(__dirname,'/dist/data-export/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

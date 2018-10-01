3e950d3d3aa01e9e4508ae5e1155b84575fe39ad
Hope Tambala <hopetambala@outlook.com>
2018-10-01 02:01:28 -0400

    feat(add latrine and roof/floor conditions):

--- src/app/pages/analysis/analysis.component.ts
+++ src/app/pages/analysis/analysis.component.ts
@@ -12,6 +12,12 @@ export class AnalysisComponent implements OnInit {
   numberOfIds = 0;
   numberofMatchingIds;
 
+  latrineAccessYes = 0;
+  latrineAccessNo = 0;
+
+  poorFloorCondition = 0;
+  poorRoofCondition = 0;
+
   constructor(private query:QueriesService) { 
     this.setup();
     
@@ -28,12 +34,49 @@ export class AnalysisComponent implements OnInit {
         //console.log(result.id)
         this.listOfAllIDs.push(result.id)
         this.numberOfIds +=1;
+
+        ///Retrieve Old Data
+        if (result.get('latrineAccess') === 'Y'){
+          this.latrineAccessYes+=1;
+        }
+        if (result.get('latrineAccess') === 'N'){
+          this.latrineAccessNo+=1;
+        }
+
+        if (result.get('conditionoFloorinyourhouse') === 'dirtPoor'){
+          this.poorFloorCondition+=1;
+        }
+        if (result.get('conditionoRoofinyourhouse') === 'poor'){
+          this.poorRoofCondition+=1;
+        }
+
       }
 
       this.query.containedQuery(2000,'HistoryEnvironmentalHealth','client',this.listOfAllIDs).then((results)=>{
         this.arrayOfMatchEnvFiles = results;
         this.numberofMatchingIds = results.length;
-        //console.log(this.arrayOfMatchEnvFiles)
+
+        //Retrieve Latrine Access info from migration
+        for (let i=0; i<this.arrayOfMatchEnvFiles.length; i++){
+          if (this.arrayOfMatchEnvFiles[i].get('latrineAccess') === 'Y'){
+            this.latrineAccessYes+=1;
+          }
+          else if (this.arrayOfMatchEnvFiles[i].get('latrineAccess') === 'N'){
+            this.latrineAccessNo+=1;
+          }
+
+        }
+        for(let i=0; i<this.arrayOfMatchEnvFiles.length; i++){
+          if (this.arrayOfMatchEnvFiles[i].get('conditionoFloorinyourhouse') === 'dirtPoor'){
+            this.poorFloorCondition+=1;
+          }
+          if (this.arrayOfMatchEnvFiles[i].get('conditionoRoofinyourhouse') === 'poor'){
+            this.poorRoofCondition+=1;
+          }
+        }
+        
+        
+
       })
 
 
